23ec17e787c50d9761b32a785dd14dfb6b93b28c
Shyam Mishra <155941220+Shyam932@users.noreply.github.com>
2025-02-17 15:13:25 +0530

    Update analyticsRoutes.js

--- backend/routes/analyticsRoutes.js
+++ backend/routes/analyticsRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const analyticsController = require('../controllers/analyticsController');
+const analyticsController = require('../Controllers/analyticsController');
 
 router.get('/financial', analyticsController.getFinancialAnalytics);
 
